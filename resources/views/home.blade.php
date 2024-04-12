17001d988ba4d2d5cde5f5648d771e32b37ecb81
ecoscu <ecosculluelarodellar@iesmordefuentes.com>
2024-04-12 16:37:04 +0200

    fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,17 @@
     </div>
     
 
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <x-dropdown-link :href="route('logout')"
+                onclick="event.preventDefault();
+                            this.closest('form').submit();">
+            {{ __('Log Out') }}
+        </x-dropdown-link>
+    </form>
+
+
     {{-- <div class="flex flex-wrap justify-evenly pb-5">
         @foreach ($contents as $content)
             <div style="background-image: url('data:image/jpeg;base64,{{ $content->base64Img }}');" class="text-left p-2">
